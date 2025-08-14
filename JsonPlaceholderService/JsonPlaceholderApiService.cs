d1b5a99a7f2f22f5538f32d0d4bd3f64ef12a92e
roy wu <136756432+wujinfen@users.noreply.github.com>
2025-08-14 14:34:44 -0400

    implemented notes HTTP methods

--- JsonPlaceholderService/JsonPlaceholderApiService.cs
+++ JsonPlaceholderService/JsonPlaceholderApiService.cs
@@ -1,17 +1,18 @@
 using JsonPlaceholderService.Models;
+using System.text;
 using System.Text.Json;
 
 namespace JsonPlaceholderService
 {
-    public class JsonPlaceHolderApiService
+    public class JsonPlaceholderApiService
     {
         private readonly HttpClient _httpClient; //recieve an HTTP Client via dependency injection so that it doesn't create a new client for every request
-        private readonly JsonSerializationOptions _jsonOptions; //serializer because the expected cases don't match
+        private readonly JsonSerializerOptions _jsonOptions; //serializer because the expected cases don't match
 
-        public JsonPlaceHolderApiService(HttpClient httpClient)
+        public JsonPlaceholderApiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _jsonOptions = new JsonSerializationOptions
+            _jsonOptions = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true,
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
@@ -23,41 +24,68 @@ namespace JsonPlaceholderService
                 _httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
             }
 
-            __httpClient.Timeout = TimeSpan.FromSeconds(15);
+            _httpClient.Timeout = TimeSpan.FromSeconds(12);
         }
 
         /** 
           * /posts HTTP methods (note: all methods are async)
           * 
+          * FOR REFERENCE: GetAsync, PostAsync, PutAsync, DeleteAsync
           * Get - all posts
           * Get - single post by id
           * Post - create new post
           * Put - update post by id
           * Delete - delete post by id
           */
-        public async Task<Post?> GetPost()
-        {
 
+        public async Task<List<Post>> GetPost()
+        {
+            var response = await _httpClient.GetAsync("posts");
+            response.EnsureSuccessStatusCode(); 
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<Post>>(content, _jsonOptions) ?? new List<Post>(); //convert JSON to Post obj, return empty list if null
         }
 
         public async Task<Post?> GetPost(int id)
         {
-
+            var response = await _httpClient.GetAsync($"posts/{id}");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Post>(content, _jsonOptions) ?? null;
         }
 
+        //might be a bit strange since JSON Placeholder doesn't create, update, or delete posts
         public async Task<Post?> CreatePost(Post post)
         {
+            //convert post to JSON and send it in request body
+            var json = JsonSerializer.Serialize(post, _jsonOptions);
+#           //create body and set header fields
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
+            //send post request to API
+            var response = await _httpClient.PostAsync("posts", content);
+            response.EnsureSuccessStatusCode();
+
+            //convert response to Post object
+            var responseJson = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Post>(responseJson, _jsonOptions);
         }
 
         public async Task<Post?> UpdatePost(int id, Post post)
         {
-
+            var json = JsonSerializer.Serialize(post, _jsonOptions);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"posts/{id}", content);
+            response.EnsureSuccessStatusCode();
+            var responseJson = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Post>(responseJson, _jsonOptions);
         }
 
         public async Task<bool> DeletePost(int id)
         {
-
+            var response = await _httpClient.DeleteAsync($"posts/{id}");
+            response.EnsureSuccessStatusCode();
+            return response.IsSuccessStatusCode;
         }
 
     }
