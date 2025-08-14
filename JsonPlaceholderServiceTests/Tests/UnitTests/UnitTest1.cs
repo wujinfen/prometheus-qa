daab7c4e45695d4b652b3aa79a3ebc0583ec6ee9
roy wu <136756432+wujinfen@users.noreply.github.com>
2025-08-14 15:57:40 -0400

    created and linked service tests NUnit project

---
+++ JsonPlaceholderServiceTests/Tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace JsonPlaceholderServiceTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
