e59417b7135147901d353e20973267660bc4e8e2
roy wu <136756432+wujinfen@users.noreply.github.com>
2025-08-16 11:44:09 -0400

    implemented playwright tests before POM refactor

---
+++ PlaywrightWebTests/UnitTest1.cs
@@ -0,0 +1,151 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+/*
+ * Playwright .NET Reference: https://playwright.dev/dotnet/docs/library
+ * 
+ */
+namespace PlaywrightWebTests
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class PrometheusWebTests : PageTest
+    {
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IBrowserContext _context;
+        private IPage _page;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 100
+            });
+            _context = await _browser.NewContextAsync();
+            _page = await _context.NewPageAsync();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            
+            if (_page != null) { await _page.CloseAsync(); } //close tab
+            if (_context != null) { await _context.CloseAsync(); } //clear session
+            if (_browser != null) { await _browser.CloseAsync(); } //close browser
+            _playwright?.Dispose(); //dispose playwright
+        }
+
+        [Test]
+        public async Task PrometheusLogoVisible()
+        {
+            await _page.GotoAsync("https://www.prometheusgroup.com/");
+            //accept cookies to avoid blocking
+            var acceptCookies = _page.Locator("#hs-eu-confirmation-button");
+            if (await acceptCookies.IsVisibleAsync())
+            {
+                await acceptCookies.ClickAsync();
+            }
+            //close popup if it appears
+            var closePopup = _page.Locator("#interactive-close-button");
+            if (await closePopup.IsVisibleAsync())
+            {
+                await _page.ScreenshotAsync(new() { Path = "popup.png" });
+                await closePopup.ClickAsync();
+            }
+            //escapes error message if it appears
+            await _page.Keyboard.PressAsync("Escape");
+
+            //check that the logo is visible
+            var logo = _page.Locator("img[src='https://www.prometheusgroup.com/hubfs/prometheus-group-srw/prometheus-group.svg']");
+            await Expect(logo).ToBeVisibleAsync();
+            await Expect(logo).ToHaveAttributeAsync("alt", "prometheus-group");
+        }
+
+        [Test]
+        public async Task PrometheusGroupContactFormValidation()
+        {
+            await _page.GotoAsync("https://www.prometheusgroup.com/");
+
+            //accept cookies to avoid blocking
+            var acceptCookies = _page.Locator("#hs-eu-confirmation-button");
+            if (await acceptCookies.IsVisibleAsync())
+            {
+                await acceptCookies.ClickAsync();
+            }
+
+            //close popup if it appears
+            var closePopup = _page.Locator("#interactive-close-button");
+            if (await closePopup.IsVisibleAsync())
+            {
+                await _page.ScreenshotAsync(new() { Path = "popup.png" });
+                await closePopup.ClickAsync();
+            }
+
+            //escapes error message if it appears
+            await _page.Keyboard.PressAsync("Escape");
+
+            //click Contact button
+            await _page.Locator("#hs-cta-182765558177-0").ClickAsync(); 
+
+            //find and fill first and last name fields
+            var firstNameField = _page.Locator("#firstname-fe70f03d-5bac-4ad3-a698-3e130182d674_8630");
+            await firstNameField.FillAsync("Roy");
+
+            var lastNameField = _page.Locator("#lastname-fe70f03d-5bac-4ad3-a698-3e130182d674_8630");
+            await lastNameField.FillAsync("G. Biv");
+
+            //submit the form
+            await _page.Locator("input[type='submit'][value='Contact Us']").ClickAsync();
+
+            //validate that the form shows 4 errors for each missing field: "Please complete this required field."
+            await Expect(_page.Locator("label.hs-error-msg.hs-main-font-element")).ToHaveCountAsync(4);
+            await _page.ScreenshotAsync(new() { Path = "required_fields.png" });
+
+            //validate that missing fields are empty: email, company, region, interested product
+            var emailField = _page.Locator("#email-fe70f03d-5bac-4ad3-a698-3e130182d674_8630");
+            await Expect(emailField).ToHaveValueAsync(string.Empty);
+            var companyField = _page.Locator("#company-fe70f03d-5bac-4ad3-a698-3e130182d674_8630");
+            await Expect(companyField).ToHaveValueAsync(string.Empty);
+            var regionField = _page.Locator("#global_region-fe70f03d-5bac-4ad3-a698-3e130182d674_8630");
+            await Expect(regionField).ToHaveValueAsync(string.Empty);
+            var productField = _page.Locator("#product-fe70f03d-5bac-4ad3-a698-3e130182d674_8630");
+            await Expect(productField).ToHaveValueAsync(string.Empty);
+        }
+
+        [Test]
+        public async Task NavigateToAboutUsPage()
+        {
+            await _page.GotoAsync("https://www.prometheusgroup.com/");
+
+            //accept cookies to avoid blocking
+            var acceptCookies = _page.Locator("#hs-eu-confirmation-button");
+            if (await acceptCookies.IsVisibleAsync())
+            {
+                await acceptCookies.ClickAsync();
+            }
+
+            //close popup if it appears
+            var closePopup = _page.Locator("#interactive-close-button");
+            if (await closePopup.IsVisibleAsync())
+            {
+                await _page.ScreenshotAsync(new() { Path = "popup.png" });
+                await closePopup.ClickAsync();
+            }
+
+            //escapes error message if it appears
+            await _page.Keyboard.PressAsync("Escape");
+
+            //hover over Company section in navbar and click About Us link
+            await _page.Locator("span.parent-link.has-dropdown:has-text('Company')").HoverAsync();
+            await _page.Locator("text='About Us'").First.ClickAsync();
+        }
+    }
+}
